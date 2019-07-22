9a8de88277eb232a21ade86644a35238bf4146b0
Chaturvedi-ji <animeshsid@ymail.com>
2019-07-22 16:55:33 +0530

    NA

---
+++ app/src/main/java/com/chaturvediji/to_doapp/empty_list.java
@@ -0,0 +1,13 @@
+package com.chaturvediji.to_doapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class empty_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty_list);
+    }
+}
