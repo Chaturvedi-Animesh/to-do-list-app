f326f95bdb8dbd3007ab3c13487a93a26ae20c5f
Chaturvedi-ji <animeshsid@ymail.com>
2019-07-22 16:50:10 +0530

    Addition of new/existing list 1

---
+++ app/src/main/java/com/chaturvediji/to_doapp/existing_or_not.java
@@ -0,0 +1,13 @@
+package com.chaturvediji.to_doapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class existing_or_not extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_existing_or_not);
+    }
+}
